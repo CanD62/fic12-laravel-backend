6b6b1593f29418b0476708d3ed39b6288f9ce258
CanD62 <puputz.online@gmail.com>
2024-01-18 11:23:43 +0700

    update...

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,21 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     //index
-    public function index(Request $request)
+    public function index()
     {
-        // $products = \App\Models\Product::paginate(5);
-        $products = DB::table('products')
-            ->leftJoin('categories', 'products.category_id', '=', 'categories.id')
-            ->when($request->input('name'), function ($query, $name) {
-                return $query->where('products.name', 'like', '%' . $name . '%');
-            })
-            ->select('products.*', 'categories.name as category_name') // Add other columns if needed
-            ->paginate(5);
+        $products = \App\Models\Product::paginate(5);
         return view('pages.product.index', compact('products'));
     }
 
@@ -44,5 +36,37 @@ class ProductController extends Controller
         $product->save();
 
         return redirect()->route('product.index');
+    }
+
+    //edit
+    public function edit($id)
+    {
+        $product = \App\Models\Product::findOrFail($id);
+        $categories = \App\Models\Category::all();
+        return view('pages.product.edit', compact('product', 'categories'));
+    }
+
+    //update
+    public function update(Request $request, $id)
+    {
+        $product = \App\Models\Product::findOrFail($id);
+        //if image is not empty, then update the image
+        if ($request->image) {
+            $filename = time() . '.' . $request->image->extension();
+            $request->image->storeAs('public/products', $filename);
+            $product->image = $filename;
+        }
+        $product->update($request->all());
+
+        return redirect()->route('product.index')->with('success', 'Product updated successfully');
+    }
+
+    //destroy
+    public function destroy($id)
+    {
+        $product = \App\Models\Product::findOrFail($id);
+        $product->delete();
+
+        return redirect()->route('product.index')->with('success', 'Product deleted successfully');
     }
 }
