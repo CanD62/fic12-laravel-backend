852dd3e4666d0e9a94ed2964c8a7ac84ac4d91ad
CanD62 <puputz.online@gmail.com>
2024-01-18 11:47:38 +0700

    import class db

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     //index
-    public function index()
+    public function index(Request $request)
     {
-        $products = \App\Models\Product::paginate(5);
+        // $products = \App\Models\Product::paginate(5);
+        $products = DB::table('products')
+            ->leftJoin('categories', 'products.category_id', '=', 'categories.id')
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('products.name', 'like', '%' . $name . '%');
+            })
+            ->select('products.*', 'categories.name as category_name') // Add other columns if needed
+            ->paginate(5);
         return view('pages.product.index', compact('products'));
     }
 
